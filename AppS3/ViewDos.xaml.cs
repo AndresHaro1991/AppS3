41e7a6ce2ab4758014ea4a2a75071d786ee9c035
Administrator <Administrator@WLGECOL001D3>
2021-10-28 22:38:03 -0500

    Add project files.

---
+++ AppS3/ViewDos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewDos : ContentPage
+    {
+        public ViewDos(string usuario, string clave)
+        {
+            InitializeComponent();
+
+            lblusuario.Text = usuario;
+            lblclave.Text = clave;
+
+            txtusuario.Text = usuario;
+            txtclave.Text = clave;
+        }
+    }
+}
